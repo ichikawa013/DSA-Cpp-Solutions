27d276d000e0fbf00958a0e5ffd1ad63dd956cb1
Piyush <pmishra4131@gmail.com>
2025-08-27 07:07:56 +0530

    Solved BinarySearch

---
+++ Arrays/BinarySearch.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+//iterative approach
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//     int n = nums.size(); 
+//     int low = 0, high = n - 1;
+
+//     while (low <= high) {
+//         int mid = low - (low - high )/ 2;
+//         if (nums[mid] == target) return mid;
+//         else if (target > nums[mid]) low = mid + 1;
+//         else high = mid - 1;
+//     }
+//     return -1;
+//     }
+// };
+
+//recursive approach
