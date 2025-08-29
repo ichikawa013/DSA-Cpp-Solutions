1c51f1d9633e3defcca85416c8f99858e79e3cc3
Piyush <pmishra4131@gmail.com>
2025-08-29 14:09:33 +0530

    Solved Number of Rotations in Sorted Array

---
+++ Arrays/NoRotations.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findKRotation(vector<int> &nums)  {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while(low < high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] <= nums[high])
+                high = mid;
+            else low = mid + 1;
+        }
+        return low;
+    }
+};
