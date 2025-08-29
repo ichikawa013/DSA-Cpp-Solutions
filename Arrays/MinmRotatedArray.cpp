bd34a60d4be6fd4284c36d899d093ca9ca3c1321
Piyush <pmishra4131@gmail.com>
2025-08-29 13:41:00 +0530

    Solved Minimum in Rotated Array

---
+++ Arrays/MinmRotatedArray.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        // int minm = INT_MAX;
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        
+
+
+        while(low < high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] <= nums[high])
+                high = mid;
+            else low = mid + 1;
+        }
+        return nums[low];
+    }
+};
